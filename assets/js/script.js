04ba386a7c9aeb316eb42106a7c64f01b1ce204b
Craig Farran <fazzer_70@hotmail.co.uk>
2024-03-04 15:04:32 +0000

    Feat: Add code to increment correct and incorrect scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,8 +45,10 @@ function checkAnswer() {
 
   if (isCorrect) {
     alert("You got it right!");
+    incrementScore();
   } else {
     alert(`You answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}!`);
+    incrementWrongAnswer();
   }
 
   runGame(calculatedAnswer[1]);
@@ -70,12 +72,19 @@ function calculateCorrectAnswer() {
   }
 }
 
+/**
+ * Gets the current score from the DOM and increments it by one.
+ */
 function incrementScore() {
-
+  let oldScore = parseInt(document.getElementById("score").innerText);
+  document.getElementById("score").innerText = ++oldScore;
 }
-
+/**
+ * Gets the current incorrect score from the DOM and increments it by one.
+ */
 function incrementWrongAnswer() {
-
+  let oldScore = parseInt(document.getElementById("incorrect").innerText);
+  document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
